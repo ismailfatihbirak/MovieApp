a0b76e585ef8cccbba652068146a561ec16cd6ba
İsmail Fatih Birak <ismailfatihbirak@gmail.com>
2024-05-16 15:30:13 +0300

    model classes

--- app/src/main/java/com/example/movieapp/domain/model/discover_popular/Discover.kt
+++ app/src/main/java/com/example/movieapp/domain/model/discover_popular/Discover.kt
@@ -1,8 +1,8 @@
 package com.example.movieapp.domain.model.discover_popular
 
 data class Discover(
-    val page: Int,
-    val results: List<Result>,
-    val total_pages: Int,
-    val total_results: Int
+    val page: Int? = null,
+    val results: List<Result> = listOf(),
+    val total_pages: Int? = null,
+    val total_results: Int? = null
 )
