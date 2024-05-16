2e1ace0106af7dba054b9cc68ba8d6ccda083df0
İsmail Fatih Birak <ismailfatihbirak@gmail.com>
2024-05-16 15:30:40 +0300

    hilt

--- app/src/main/java/com/example/movieapp/data/di/AppModule.kt
+++ app/src/main/java/com/example/movieapp/data/di/AppModule.kt
@@ -1,6 +1,8 @@
 package com.example.movieapp.data.di
 
 import com.example.movieapp.data.remote.MovieApi
+import com.example.movieapp.data.repository.MovieRepoImpl
+import com.example.movieapp.domain.repository.MovieRepo
 import com.example.movieapp.util.Constant.BASE_URL
 import dagger.Module
 import dagger.Provides
@@ -13,14 +15,18 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
     @Provides
     @Singleton
-    fun provideUniversityApi() : MovieApi {
-        return Retrofit.Builder().
-        baseUrl(BASE_URL)
+    fun provideUniversityApi(): MovieApi {
+        return Retrofit.Builder().baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(MovieApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUniversityRepo(api: MovieApi): MovieRepo {
+        return MovieRepoImpl(api = api)
     }
 }
