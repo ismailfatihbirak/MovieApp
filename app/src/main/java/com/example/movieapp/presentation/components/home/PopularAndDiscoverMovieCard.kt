61533a11781229494bbe0bf5b82a93ae616dd1ce
İsmail Fatih Birak <ismailfatihbirak@gmail.com>
2024-05-16 15:43:21 +0300

    bug fix

--- app/src/main/java/com/example/movieapp/presentation/components/home/PopularAndDiscoverMovieCard.kt
+++ app/src/main/java/com/example/movieapp/presentation/components/home/PopularAndDiscoverMovieCard.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import com.example.movieapp.presentation.home.CustomText
 
 @Composable
 fun PopularAndDiscoverMovieCard(
