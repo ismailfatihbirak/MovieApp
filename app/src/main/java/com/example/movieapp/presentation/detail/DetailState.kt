27d8d445704ae64d6b560f40226432256b3b1b02
İsmail Fatih Birak <ismailfatihbirak@gmail.com>
2024-05-27 22:22:28 +0300

    detail

--- app/src/main/java/com/example/movieapp/presentation/detail/DetailState.kt
+++ app/src/main/java/com/example/movieapp/presentation/detail/DetailState.kt
@@ -1,11 +1,9 @@
 package com.example.movieapp.presentation.detail
 
 import com.example.movieapp.domain.model.detail.Detail
-import com.example.movieapp.domain.model.videos.Videos
 
 data class DetailState(
     val isLoading: Boolean = false,
     val movie: Detail = Detail(),
-    val video: Videos = Videos(),
     val error: String = "",
 )
--- app/src/main/java/com/example/movieapp/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/movieapp/presentation/detail/DetailViewModel.kt
@@ -5,8 +5,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.movieapp.domain.use_case.get_detail_movie.GetDetailMovieUseCase
-import com.example.movieapp.domain.use_case.get_videos_movie.GetVideosMovieUseCase
-import com.example.movieapp.presentation.home.HomeState
 import com.example.movieapp.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
@@ -16,14 +14,11 @@ import javax.inject.Inject
 @HiltViewModel
 class DetailViewModel @Inject constructor(
     private val getDetailMovieUseCase: GetDetailMovieUseCase,
-    private val getVideosMovieUseCase: GetVideosMovieUseCase
 ) : ViewModel() {
 
     private val _detailState = mutableStateOf<DetailState>(DetailState())
     val detailState: State<DetailState> = _detailState
 
-    private val _videosState = mutableStateOf<DetailState>(DetailState())
-    val videosState: State<DetailState> = _videosState
 
     private fun getDetailMovie(movieId: String) {
         getDetailMovieUseCase.executeGetDetailMovie(movieId).onEach {
@@ -43,31 +38,10 @@ class DetailViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
-    private fun getVideosMovie(movieId: String) {
-        getVideosMovieUseCase.executeGetVideosMovie(movieId = movieId) .onEach {
-            when (it) {
-                is Resource.Success -> {
-                    _videosState.value = it.data?.let { it1 -> DetailState(video = it1) }!!
-                }
-
-                is Resource.Loading -> {
-                    _videosState.value = DetailState(isLoading = true)
-                }
-
-                is Resource.Error -> {
-                    _videosState.value = DetailState(error = it.message ?: "Error")
-                }
-            }
-        }.launchIn(viewModelScope)
-    }
-
 
     fun loadGetDetailMovie(movieId: String) {
         getDetailMovie(movieId)
     }
 
-    fun loadGetVideosMovie(movieId: String) {
-        getVideosMovie(movieId)
-    }
 
 }
