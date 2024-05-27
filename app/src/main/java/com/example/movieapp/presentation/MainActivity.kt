b6ce64c90de46ef136eb8f188b69e70edbcc1bec
İsmail Fatih Birak <ismailfatihbirak@gmail.com>
2024-05-27 22:24:08 +0300

    main activity

--- app/src/main/java/com/example/movieapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +13,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.movieapp.R
 import com.example.movieapp.presentation.detail.DetailScreen
 import com.example.movieapp.presentation.home.HomeScreen
-import com.example.movieapp.presentation.ui.theme.MovieAppTheme
+import com.example.movieapp.presentation.splash.SplashScreen
+import com.example.movieapp.presentation.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,13 +34,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MovieAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ScreenTransations()
-                }
+            AppTheme {
+                ScreenTransations()
             }
         }
     }
@@ -44,7 +44,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ScreenTransations() {
     val navController = rememberNavController()
-    NavHost(navController, startDestination = "home") {
+    NavHost(navController, startDestination = "splash") {
+        composable("splash") { backStackEntry ->
+            SplashScreen(navController)
+        }
         composable("home") { backStackEntry ->
             HomeScreen(navController = navController)
         }
