485d414a96fbd10202a9656b609c33d9af12427c
İsmail Fatih Birak <ismailfatihbirak@gmail.com>
2024-05-27 22:25:30 +0300

    delete import

--- app/src/main/java/com/example/movieapp/presentation/home/HomeState.kt
+++ app/src/main/java/com/example/movieapp/presentation/home/HomeState.kt
@@ -1,8 +1,6 @@
 package com.example.movieapp.presentation.home
 
-import com.example.movieapp.domain.model.detail.Detail
 import com.example.movieapp.domain.model.discover_popular.Discover
-import com.example.movieapp.domain.model.videos.Videos
 
 class HomeState (
     val isLoading: Boolean = false,
